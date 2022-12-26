5e2b9d889b3244eacb5d1b468e234687874bef34
Nina Fuentes <nina.fuentes.n@gmail.com>
2022-12-25 19:55:00 -0500

    Option to delete task OR mark as complete. Modified the hide complet button

--- src/components/Task.js
+++ src/components/Task.js
@@ -1,10 +1,20 @@
-import { FaTimes } from 'react-icons/fa'
+import { FaCheckCircle, FaTimes, FaRegCircle } from 'react-icons/fa'
 
 const task = ({task, onDelete, onToggle, showCompleted}) => {
+
+  const status = (function() {
+    if (task.completed === true ) {
+      return <FaCheckCircle style={{ color: 'green', cursor: 'pointer'}} onClick={() => onToggle(task.id)} />
+    } else {
+    return  <FaRegCircle style={{ color: 'black', cursor: 'pointer'}} onClick={() => onToggle(task.id)} />
+    }
+  })()
+
   return (
-    <div className={`task ${task.completed ? 'reminder' : ''}`} onDoubleClick={() => onToggle(task.id)} style={{display: (task.completed === true && showCompleted === true) && 'none'}} >
-        <h3>{task.title} <FaTimes style={{ color: 'red', cursor: 'pointer'}} onClick={() => onDelete(task.id)}/></h3>
+    <div className={`task ${task.completed ? 'reminder' : ''}`} onDoubleClick={() => onToggle(task.id)} style={{cursor:'default', display: (task.completed === true && showCompleted === true) && 'none'}} >
+        <h3>{task.title} {status}</h3>
         <p>{task.description}</p>
+        <button className='btn btn-delete' onClick={() => onDelete(task.id)} >Delete Task</button>
     </div>
   )
 }
